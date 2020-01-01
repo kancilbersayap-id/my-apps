5f4971a693f82ae3a9708ab67114c3bb9ca38007
Fariz Maulana <farizpie@Farizs-MacBook-Pro.local>
2020-01-02 05:48:48 +0700

    Watch the video hyperlink

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'This is a header',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Learn to Design and code with React</h1>
+    <p>I think it's good as designer have a Front End skills</p>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
